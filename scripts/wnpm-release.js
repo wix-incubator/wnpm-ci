78e36045f59f3aabf9a327141129bdcbe846acbd
Idan Attias <idan-at@users.noreply.github.com>
2018-05-21 21:24:07 +0300

    Support npm pack quietly (#13)

--- scripts/wnpm-release.js
+++ scripts/wnpm-release.js
@@ -3,10 +3,13 @@
 
 var shouldShrinkWrap = process.argv.indexOf("--no-shrinkwrap") < 0;
 var shouldPublishToWixRegistry = process.argv.indexOf("--publish-to-wix-registry") >= 0;
+var shouldPackQuietly = process.argv.indexOf("--pack-quietly") >= 0;
 
 require('../index').prepareForRelease({
   shouldShrinkWrap: shouldShrinkWrap,
-  shouldPublishToWixRegistry: shouldPublishToWixRegistry}, function(err) {
+  shouldPublishToWixRegistry: shouldPublishToWixRegistry,
+  shouldPackQuietly: shouldPackQuietly
+}, function(err) {
   if (err) {
     console.log(err);
     process.exit(1);

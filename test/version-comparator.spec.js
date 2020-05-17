322442a9cb2ca5dfaeca5ffd236aef8c7ef68803
idok <idok@wix.com>
2020-05-17 17:15:35 +0300

    no semi

--- test/version-comparator.spec.js
+++ test/version-comparator.spec.js
@@ -1,18 +1,18 @@
-const fs = require('fs');
-const path = require('path');
-const {expect} = require('chai');
-const {compare} = require('../lib/version-comparator');
-const versionFetcher = require('../lib/version-fetcher');
+const fs = require('fs')
+const path = require('path')
+const {expect} = require('chai')
+const {compare} = require('../lib/version-comparator')
+const versionFetcher = require('../lib/version-fetcher')
 
 describe('version-comparator', () => {
   it('should compare version with itself', async () => {
-    const cwd = await versionFetcher.fetch('wnpm-ci', '6.2.0');
-    expect(await compare(cwd, '6.2.0')).to.equal(true);
-  });
+    const cwd = await versionFetcher.fetch('wnpm-ci', '6.2.0')
+    expect(await compare(cwd, '6.2.0')).to.equal(true)
+  })
 
   it('should return false if we have difference', async () => {
-    const cwd = await versionFetcher.fetch('wnpm-ci', '6.2.0');
+    const cwd = await versionFetcher.fetch('wnpm-ci', '6.2.0')
     fs.writeFileSync(path.join(cwd, 'abc'), '111')
-    expect(await compare(cwd, '6.2.0')).to.equal(false);
-  });
-});
+    expect(await compare(cwd, '6.2.0')).to.equal(false)
+  })
+})

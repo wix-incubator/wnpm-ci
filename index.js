04a24e176d23bfc5f69c5e3cb1fe278d3d1fb1ae
Ido <idok@wix.com>
2020-05-17 17:07:57 +0300

    add check hash (#40)

--- index.js
+++ index.js
@@ -23,18 +23,18 @@ async function findPublishedVersionsOnAllRegistries(cwd, registries) {
     registries = ['https://npm.dev.wixpress.com/', 'https://registry.npmjs.org/'];
   }
 
-  let packageInfoPromises = [];
+  const packageInfoPromises = [];
   for (const registry of registries) {
     packageInfoPromises.push(maybeGetPackageInfo(scopedPackageName, registry));
 
     if (!registry.includes('registry.npmjs.org') ||
       (pkg.publishConfig && pkg.publishConfig.registry === 'https://registry.npmjs.org/')) {
-        packageInfoPromises.push(maybeGetPackageInfo(unscopedPackageName, registry));
+      packageInfoPromises.push(maybeGetPackageInfo(unscopedPackageName, registry));
     }
   }
 
   const packagesInfo = (await Promise.all(packageInfoPromises)).filter(pkgInfo => !!pkgInfo);
-  
+
   const versions = packagesInfo.reduce((acc, pkgInfo) => {
     const pkgVersions = normalizeVersions(pkgInfo.versions);
     return acc.concat(pkgVersions);
@@ -44,11 +44,11 @@ async function findPublishedVersionsOnAllRegistries(cwd, registries) {
 
   // This is just to report stats from all registries
   const currentPublishedVersion = versionCalculations.calculateCurrentPublished(pkg.version, uniqueVersions);
-  console.log(`currentPublishedVersion`, currentPublishedVersion);
+  console.log('currentPublishedVersion', currentPublishedVersion);
   packagesInfo.forEach(pkgInfo => {
     const registry = pkgInfo.dist.tarball.split('/').slice(0,3).join('/');
     console.log(`registry: ${registry} pkgName: ${pkgInfo.name}`);
-  })
+  });
 
   return uniqueVersions;
 }
@@ -64,7 +64,8 @@ function normalizeVersions(versions) {
 }
 
 async function isSameAsPublished(registryVersions, options) {
-  const localPackageVersion = (await packageHandler.readPackageJson(path.join(options.cwd, 'package.json'))).version;
+  const pkg = await packageHandler.readPackageJson(path.join(options.cwd, 'package.json'));
+  const localPackageVersion = pkg.version;
   const currentPublishedVersion = versionCalculations.calculateCurrentPublished(localPackageVersion, registryVersions, options);
 
   if (!currentPublishedVersion) {
@@ -74,15 +75,15 @@ async function isSameAsPublished(registryVersions, options) {
   let isSame = false;
 
   if (options.versionToCompare) {
-    isSame = await compareVersions(options.cwd, options.versionToCompare, currentPublishedVersion)
+    isSame = await compareVersions(options.cwd, options.versionToCompare, currentPublishedVersion, options);
   } else {
-    isSame = await compare(options.cwd, currentPublishedVersion)
+    isSame = await compare(options.cwd, currentPublishedVersion, options);
   }
 
   if (isSame) {
-    return currentPublishedVersion
+    return currentPublishedVersion;
   } else {
-    return false
+    return false;
   }
 }
 
@@ -100,9 +101,13 @@ async function writePackageVersion(version, cwd) {
   await execa(`npm version --no-git-tag-version ${version}`, {shell: true, cwd});
 }
 
-async function prepareForRelease(options) {
-  options = options || {};
+/**
+ * @param {{shouldBumpMinor?:boolean, checkHashInPackageJson?:boolean, cwd?:string, registries?:string[]}} [options]
+ * @returns {Promise<void>}
+ */
+async function prepareForRelease(options = {}) {
   options.cwd = options.cwd || process.cwd();
+  options.registries = options.registries || ['https://npm.dev.wixpress.com/', 'https://registry.npmjs.org/'];
 
   const pkg = await packageHandler.readPackageJson(path.join(options.cwd, 'package.json'));
 
@@ -124,9 +129,9 @@ async function prepareForRelease(options) {
       currentPublishedVersion = await isSameAsPublished(registryVersions, options);
     }
     catch (err) {
-      console.log("An error has occurred while comparing current version to published version:");
+      console.log('An error has occurred while comparing current version to published version:');
       console.log(err.stack || err);
-      console.log("Since comparing the versions has failed, a new version will still be released!");
+      console.log('Since comparing the versions has failed, a new version will still be released!');
       currentPublishedVersion = false;
     }
 
--- test/wnpm-release.spec.js
+++ test/wnpm-release.spec.js
@@ -40,7 +40,7 @@ describe('wnpm-release', () => {
 
   it('should not touch version if it was modifier manually', async () => {
     const cwd = await versionFetcher.fetch('wnpm-ci', '6.2.0');
-    execSync(`npm version --no-git-tag-version 6.5.0`, {cwd});
+    execSync('npm version --no-git-tag-version 6.5.0', {cwd});
     await prepareForRelease({cwd, shouldBumpMinor: true});
 
     const pkg = await packageHandler.readPackageJson(path.join(cwd, 'package.json'));
@@ -62,7 +62,7 @@ describe('wnpm-release', () => {
   it('should bump version if comparing to published version fails', async () => {
     const cwd = await versionFetcher.fetch('wnpm-ci', '6.2.0');
     const originalFetch = versionFetcher.fetch;
-    versionFetcher.fetch = () => Promise.reject(new Error("Failed!"));
+    versionFetcher.fetch = () => Promise.reject(new Error('Failed!'));
     await prepareForRelease({cwd});
     versionFetcher.fetch = originalFetch;
 
@@ -76,23 +76,23 @@ describe('wnpm-release', () => {
     it('should not bump a version when comparing lastest to the matching version', async () => {
       const cwd = await versionFetcher.fetch('tmp.xsb6m4j2', '1.0.0');
       await prepareForRelease({ cwd, versionToCompare: 'https://registry.npmjs.org/tmp.xsb6m4j2/-/tmp.xsb6m4j2-1.0.0-same.tgz'});
-  
+
       const pkg = await packageHandler.readPackageJson(path.join(cwd, 'package.json'));
       expect(pkg.private).to.equal(true);
-    })
+    });
 
     it('should bump a version when comparing lastest to a non matching version', async () => {
       const cwd = await versionFetcher.fetch('tmp.xsb6m4j2', '1.0.0');
       await prepareForRelease({ cwd, versionToCompare: 'https://registry.npmjs.org/tmp.xsb6m4j2/-/tmp.xsb6m4j2-1.0.0-not-same.tgz'});
-  
+
       const pkg = await packageHandler.readPackageJson(path.join(cwd, 'package.json'));
       expect(pkg.private).to.equal(undefined);
-      expect(pkg.version).to.equal('1.0.1')
-    })
-  })
+      expect(pkg.version).to.equal('1.0.1');
+    });
+  });
 
   describe('with custom registry', () => {
-    let registry = aRegistryDriver();
+    const registry = aRegistryDriver();
 
     before(() =>
       registry.start());
@@ -101,18 +101,62 @@ describe('wnpm-release', () => {
       registry.stop());
 
     it('should use a different set of registries if passed, when fetching version for bump', async () => {
-      const packageName = `cool-npm-package`;
+      const packageName = 'cool-npm-package';
       const [olderVersion, mostRecentVersion, nextVersion] = ['4.5.23', '4.5.24', '4.5.25'];
       await registry.putPackageInRegistry({ packageName, version: olderVersion });
       await registry.putPackageInRegistry({ packageName, version: mostRecentVersion });
 
       const cwd = await registry.fetchPackage({ packageName, version: olderVersion });
       await prepareForRelease({ cwd, registries: [registry.getRegistryUrl()] });
-  
+
       const pkg = await packageHandler.readPackageJson(path.join(cwd, 'package.json'));
       expect(pkg.version).to.equal(nextVersion);
     });
-  })
+  });
+
+  describe('with checkHashInPackageJson', function () {
+    this.timeout(50000);
+    const registry = aRegistryDriver();
+    const packageName = 'cool-npm-package';
+    const [olderVersion, mostRecentVersion, nextVersion] = ['4.5.23', '4.5.24', '4.5.25'];
+
+    beforeEach(() => registry.start());
+
+    afterEach(() => registry.stop());
+
+    it('should bump on diffrent checksum', async () => {
+      await registry.putPackageInRegistry({ packageName, version: olderVersion, checksum: 'a' });
+      await registry.putPackageInRegistry({ packageName, version: mostRecentVersion, checksum: 'b' });
+
+      const cwd = await registry.fetchPackage({ packageName, version: olderVersion });
+      await prepareForRelease({ cwd, registries: [registry.getRegistryUrl()] , checkHashInPackageJson: true});
+
+      const pkg = await packageHandler.readPackageJson(path.join(cwd, 'package.json'));
+      expect(pkg.version).to.equal(nextVersion);
+    });
+
+    it('should not publish on same checksum local', async () => {
+      await registry.putPackageInRegistry({ packageName, version: olderVersion, checksum: 'a' });
+      await registry.putPackageInRegistry({ packageName, version: mostRecentVersion, checksum: 'a' });
+
+      const cwd = await registry.fetchPackage({ packageName, version: olderVersion });
+      await prepareForRelease({ cwd, registries: [registry.getRegistryUrl()] , checkHashInPackageJson: true});
+
+      const pkg = await packageHandler.readPackageJson(path.join(cwd, 'package.json'));
+      expect(pkg.version).to.equal(mostRecentVersion);
+    });
+
+    it('should not publish on same checksum versions', async () => {
+      await registry.putPackageInRegistry({ packageName, version: olderVersion, checksum: 'a' });
+      await registry.putPackageInRegistry({ packageName, version: mostRecentVersion, checksum: 'a' });
+
+      const cwd = await registry.fetchPackage({ packageName, version: olderVersion });
+      await prepareForRelease({ cwd, registries: [registry.getRegistryUrl()] , checkHashInPackageJson: true, versionToCompare: olderVersion});
+
+      const pkg = await packageHandler.readPackageJson(path.join(cwd, 'package.json'));
+      expect(pkg.version).to.equal(mostRecentVersion);
+    });
+  });
 });
 
 describe('wnpm-release cli', () => {
